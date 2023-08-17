be452d702d408ecea72f7de255287860f5df7262
AmirMuse <amirlearn08@gmail.com>
2023-08-16 22:05:39 -0700

    Register Expense Api  Was Added

--- template/Application/api/expense.php
+++ template/Application/api/expense.php
@@ -3,4 +3,26 @@ include('../config/conn.php');
 // insert api 
 //
 
+function registerExpense($con){
+    $message = array();
+    extract($_POST);
+    $query    = "call registerExpense('','$amount','$type','$descripton','User12','')";
+    $result  = $con->query($query);
+    if($result){
+        $message = array('status'=>true,'data'=>"Expense Registered Success ");
+    }else{
+        $message = array('status'=>false,'data'=>$con->error);
+    }
+
+    echo json_encode($message);
+}
+
+if(isset($_POST['action'])){
+    $action =  $_POST['action'];
+    $action($con);
+}else{
+    echo json_encode(array('status'=>false,'data'=>'action required !!'));
+}
+
+
 ?>
