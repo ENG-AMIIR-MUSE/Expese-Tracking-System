677b78ed76a582cbb62bd491b83757bc84c6a7b2
AmirMuse <amirlearn08@gmail.com>
2023-08-19 21:14:56 -0700

    i added the update api and its logic so  User can update his transactions now

--- template/Application/api/expense.php
+++ template/Application/api/expense.php
@@ -13,7 +13,7 @@ if(isset($_POST['action'])){
 function registerExpense($con){
     $data = array();
     extract($_POST);
-    $query    = "call registerExpense('','$amount','$type','$descripton','user1','')";
+    $query    = "call registerExpense('','$amount','$type','$descripton','user1','','$oper')";
     $result  = $con->query($query);
     // check result
     if($result){
@@ -29,6 +29,25 @@ function registerExpense($con){
 
     echo json_encode($data);
 };
+function update($con){
+    $data = array();
+    extract($_POST);
+    $query    = "call registerExpense('$id','$amount','$type','$descripton','user1','','$oper')";
+    $result  = $con->query($query);
+    // check result
+    if($result){
+        $row  = $result->fetch_assoc();
+        if(isset($row['msg'])){
+        $data = array('status'=>false,'data'=>$row['msg']);
+        }else{
+            $data = array('status'=>true,'data'=>"Expense Updated  Success ");
+        }
+    }else{
+        $data = array('status'=>false,'data'=>$con->error);
+    }
+
+    echo json_encode($data);
+};
 // display balance of the user 
 
 
@@ -36,6 +55,22 @@ function registerExpense($con){
 function readAllTransaction($conn){
     $data  = arraY();
     $query = "select * from exp ";
+    $result  = $conn->query($query);
+    if($result){
+        while($row  = $result->fetch_assoc()){
+            $data[] = $row;
+        }
+        $data = array('status'=>true, 'data'=>$data);
+    }else{
+        $data  = array('status'=>false, 'data'=>$conn->error);
+    }
+    echo json_encode($data);
+}
+// 
+function readOneTransaction($conn){
+    $data  = arraY();
+    extract($_POST);
+    $query = "select * from exp  where exp.id =  '$id' ";
     $result  = $conn->query($query);
     if($result){
         while($row  = $result->fetch_assoc()){
--- template/Application/js/main.js
+++ template/Application/js/main.js
@@ -1,131 +1,192 @@
 readTransactions()
-$("#addNew").on('click',function(){
+$("#addNew").on('click', function () {
     console.log('clicked')
-$("#modal").show()
+    $("#modal").show()
 })
 
-$('#close').on('click',function(){
+$('#close').on('click', function () {
     $('#modal').hide()
 })
+let btnAction  = "insert"
 
-$('#expenseForm').on('submit',function(event){
+$('#expenseForm').on('submit', function (event) {
     event.preventDefault()
-   
-    // let formData  = new FormData($("#expenseForm")[0])
-    let amount  = $('#amount').val()
-    let type  = $('#type').val()
-    let description  = $('#desc').val()
-    let data  = {
-        'amount':amount,
-        'type':type,
-        'descripton':description,
-        'action':'registerExpense'
-    } 
-    $.ajax({
-        method:'POST',
-        dataType:"JSON",
-        url:'../api/expense.php',
-        data:data,
+    let data  = {}
 
-        success:function(data){
-           let response  = data.data 
-           displayMessage("success",response)
-           readTransactions()
-     
+    // let formData  = new FormData($("#expenseForm")[0])
+    
+    let id = $('#id').val()
+    let amount = $('#amount').val()
+    let type = $('#type').val()
+    let description = $('#desc').val()
+    if(btnAction == "insert"){
+         data = {
+            'amount': amount,
+            'type': type,
+            'descripton': description,
+            'oper':'insert',
+            'action': 'registerExpense'
+        }
+    }else{
+         data = {
+            'id':id,
+            'amount': amount,
+            'type': type,
+            'descripton': description,
+            'oper':'update',
+            'action': 'update'
+        }
+    }
+ 
+    $.ajax({
+        method: 'POST',
+        dataType: "JSON",
+        url: '../api/expense.php',
+        data: data,
+
+        success: function (data) {
+            let response = data.data
+            console.log("message: ",response)
+            displayMessage("success", response)
+            readTransactions()
+            btnAction = 'insert'
+
+
         },
-        error:function(data){
-           let response  = data.data 
-            displayMessage("error",response)
+        error: function (data) {
+            let response = data
+            console.log("message: ",response)
+
+            displayMessage("error", response)
 
         }
     })
 })
 
-function displayMessage(type,message){
-    
-    let success  = document.querySelector('.alert-success')
-    let error  = document.querySelector('.alert-danger')
+function displayMessage(type, message) {
+ 
 
-    if(type == "success"){
+    let success = document.querySelector('.alert-success')
+    let error = document.querySelector('.alert-danger')
+
+    if (type == "success") {
         success.classList = "alert alert-success "
         success.innerHTML = message
         error.classList = "alert alert-danger d-none "
         $('#modal').hide()
         $('#expenseForm')[0].reset()
-        setTimeout(()=>{
-        $('.alert-success').hide()
-        },2000)
-    }else{
+        
+        setTimeout(() => {
+            $('.alert-success').hide()
+        }, 1000)
+    } else {
         error.classList = "alert alert-danger  "
         error.innerHTML = message
         success.classList = "alert alert-success d-none "
         $('#modal').hide()
         $('#expenseForm')[0].reset()
 
-        setTimeout(()=>{
+        setTimeout(() => {
             $('.alert-danger').hide()
-            },2000)
+        }, 1000)
 
     }
 }
 
 // read the tranactions 
-function readTransactions(){
+function readTransactions() {
     $("#myTable tbody").html("")
-    let sendngData  = {
-        "action":"readAllTransaction"
+    let sendngData = {
+        "action": "readAllTransaction"
     }
     $.ajax({
-        method:'POST',
-        url:'../api/expense.php',
-        dataType:'JSON',
-        data:sendngData,
-        success:function(data){
-            console.log("Here is the data form tranaction  :",data)
-            let response  = data.data;
-            response.forEach((item)=>{
+        method: 'POST',
+        url: '../api/expense.php',
+        dataType: 'JSON',
+        data: sendngData,
+        success: function (data) {
+            // console.log("Here is the data form tranaction  :", data)
+            let response = data.data;
+            // console.log("transaction data  :", data)
+            response.forEach((item) => {
 
                 // halkaan error iga hatto ha ilawin  also 
 
-                tr  = "<tr>"
-               for(i in item){
-                if(i == "type"){
-                    console.log(i)
-                    console.log(item[i])
-                    //a ustaad shara casil  ui ui ui uilimate rea
-                    // safa adsad
-                    if(item[i] == "income"){
-                        
-                     tr+=  "<td class= 'badge badge-success '> "+ item[i]+  "</td>"
+                tr = "<tr>"
+                for (i in item) {
+                    if (i == "type") {
 
+                        //a ustaad shara casil  ui ui ui uilimate rea
+                        // safa adsad
+                        if (item[i] == "income") {
+
+                            tr += "<td class= 'badge badge-success '> " + item[i] + "</td>"
+
+                        }
+                        if (item[i] == "expense") {
+
+                            tr += "<td class= 'badge badge-danger '> " + item[i] + "</td>"
+
+                        }
+                    } else {
+
+                        tr += "<td> " + item[i] + "</td>"
                     }
-                     if(item[i] == "expense"){
-                        
-                     tr+=  "<td class= 'badge badge-danger '> "+ item[i]+  "</td>"
 
-                    }
-                }else{
 
-                    tr+=  "<td> "+ item[i]+  "</td>"
                 }
-
-
-            }
-            tr+=
-            ` <td>
-           <button class='btn btn-primary update' updateInfo = ${item['std_id']}><i class="fa-solid fa-pen-to-square"></i></button>
+                tr +=
+                    ` <td>
+           <a class='btn btn-primary text-white  update' updateInfo = ${item['id']}><i class="fa-solid fa-pen-to-square"></i></a>
            </td> `
-           tr+=
-            ` <td>
-           <button class='btn btn-danger delete' deleteInfo = ${item['std_id']}><i class="fa-solid fa-trash"></i></button>
+                tr +=
+                    ` <td>
+           <a class='btn btn-danger text-white delete' deleteInfo = ${item['id']}><i class="fa-solid fa-trash"></i></a>
            </td> `
-               tr+= "</tr>"
-            //    console.log("single row ",tr)
-               $('#myTable tbody').append(tr)
+
+                tr += "</tr>"
+                //    console.log("single row ",tr)
+                $('#myTable tbody').append(tr)
             })
         },
-        error:function(data){
-            console.log("here is the error form the transaciton :",data)
+        error: function (data) {
+            console.log("here is the error form the transaciton :", data.data)
         }
     })
 }
+
+$('#myTable').on('click', 'a.update', function () {
+    // tabel on  click  a.update  on click an anchor tag which has that class 
+    // console.log("update is clicked ")
+
+    let id = $(this).attr('updateInfo')
+
+    loadOneTransAction(id)
+    $('#modal').show()
+})
+function loadOneTransAction(id) {
+
+    let sendingData = {
+        action: 'readOneTransaction',
+        id: id,
+        
+    }
+    $.ajax({
+        method: 'POST',
+        url: '../api/expense.php',
+        dataType: 'JSON',
+        data: sendingData,
+        success: function (data) {
+            let id = $('#id').val(data.data[0].id)
+            let amount = $('#amount').val(data.data[0].amount)
+            let type = $('#type').val(data.data[0].type)
+            let description = $('#desc').val(data.data[0].description)
+            btnAction = "update"
+        },
+        error: function (data) {
+            console.log(data)
+        }
+    })
+}
+
+
